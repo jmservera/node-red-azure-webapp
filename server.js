e4a7fb878cb2dbc819d20bd53f85bc45b1ca6ff8
Juan Manuel Servera <jmservera@outlook.com>
2017-03-24 19:32:46 +0100

    remove unused things

--- server.js
+++ server.js
@@ -6,20 +6,6 @@ var app= express();
 
 const PORT=process.env.PORT||8000;
 
-// //We need a function which handles requests and send response
-// function handleRequest(request, response){
-//     console.log("call");
-//     RED.app(request,response);
-//     console.log("end");
-// }
-
-
-// //Lets start our server
-// server.listen(PORT, function(){
-//     //Callback triggered when server is successfully listening. Hurray!
-//     console.log("Server listening on: http://localhost:%s", PORT);
-// });
-
 var settings = {
     httpAdminRoot:"/",
     httpNodeRoot: "/api",
@@ -30,8 +16,10 @@ var settings = {
 var server=http.createServer(app);
 
 RED.init(server,settings);
- app.use(settings.httpAdminRoot,RED.httpAdmin);
- app.use(settings.httpNodeRoot,RED.httpNode);
 
- server.listen(PORT);
+app.use(settings.httpAdminRoot,RED.httpAdmin);
+app.use(settings.httpNodeRoot,RED.httpNode);
+
+
+server.listen(PORT);
 RED.start();
