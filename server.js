abc1bf34cd7380adf04db653067cc1ce09d45c86
Juan Manuel Servera <jmservera@outlook.com>
2017-03-25 21:03:34 +0100

    just space removal

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ RED.init(server,settings);
 
 app.use(settings.httpAdminRoot,RED.httpAdmin);
 app.use(settings.httpNodeRoot,RED.httpNode);
-server.listen(settings.uiPort);
+ 
+ server.listen(settings.uiPort);
 console.log(`listening port:${settings.uiPort}`);
 RED.start();
