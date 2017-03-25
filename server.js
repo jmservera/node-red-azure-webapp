3f1d792d61f51534c6060e29d5a681fb7f41d03e
Juan Manuel Servera <jmservera@outlook.com>
2017-03-25 20:56:23 +0100

    change settings

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+
 var express = require("express");
 var RED=require('node-red');
 var app= express();
@@ -6,20 +7,13 @@ var app= express();
 
 const PORT=process.env.PORT||8000;
 
-var settings = {
-    httpAdminRoot:"/",
-    httpNodeRoot: "/api",
-    userDir:"./.nodered/",
-    functionGlobalContext: { }    // enables global context
-};
-
 var server=http.createServer(app);
+var settings=require("./settings.js");
 
 RED.init(server,settings);
 
 app.use(settings.httpAdminRoot,RED.httpAdmin);
 app.use(settings.httpNodeRoot,RED.httpNode);
-
-
-server.listen(PORT);
+server.listen(settings.uiPort);
+console.log(`listening port:${settings.uiPort}`);
 RED.start();
